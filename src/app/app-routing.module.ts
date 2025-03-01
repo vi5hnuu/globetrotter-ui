8b5851f0effc61ffddf13aa233dc9fdbe056db95
vishnu kumar <kumarvishnu1619@gmail.com>
2025-03-02 01:46:58 +0530

    changed to hash based routing..

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{onSameUrlNavigation:'reload'})],
+  imports: [RouterModule.forRoot(routes,{onSameUrlNavigation:'reload',useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
